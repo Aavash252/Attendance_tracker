32efa23c83fe99a4e4ab1d7ce908372a9e55490f
Aavash <Dahal.aavash2520@gmail.com>
2023-08-25 14:37:04 +0530

    Updated

--- Controllers/TimeController.cs
+++ Controllers/TimeController.cs
@@ -44,6 +44,8 @@ namespace FinalProject.Controllers
                 ViewBag.ActiveClockInTime = activeClockInRecord.Clock_In;
             }
 
+            ViewBag.CurrentUserId = currentUserId; // Add this line to pass the user ID to the view
+
             var finalprojectDbContext = _context.TimeModel.Include(t => t.FinalProjectUser);
             return View(await finalprojectDbContext.ToListAsync());
         }
@@ -155,6 +157,49 @@ namespace FinalProject.Controllers
 
             return Json(StatusMessage);
         }
+
+
+        public IActionResult Graph(string userId)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Id == userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            DateTime thirtyDaysAgo = DateTime.UtcNow.AddDays(-30);
+
+            var timeRecords = _context.TimeModel
+                .Where(t => t.UserId == userId && t.Date >= thirtyDaysAgo)
+                .ToList();
+
+            var groupedRecords = timeRecords
+                .GroupBy(t => (t.Date - thirtyDaysAgo).Days + 1)
+                .Select(group => new
+                {
+                    Day = group.Key,
+                    Date = thirtyDaysAgo.AddDays(group.Key - 1)
+                        .ToString("yyyy-MM-dd"),
+                    TotalHours = group.Sum(record => (record.Clock_Out - record.Clock_In)?.TotalHours ?? 0)
+                })
+                .ToList();
+
+
+            List<string> dates = groupedRecords.Select(record => record.Date).ToList();
+            List<double> totalHours = groupedRecords.Select(record => record.TotalHours).ToList();
+
+
+            ViewBag.Dates = dates;
+            ViewBag.TotalHours = totalHours;
+
+
+            return View("Graph");
+        }
+
+
+
+
 
 
 
